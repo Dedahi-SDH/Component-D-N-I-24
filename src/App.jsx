bb68dcc0901e0a9d4d6e6f23c1f85befbaadae8e
unknown <Dedahisdh@gmail.com>
2025-12-03 03:19:53 +0000

    Ajout des fichiers du projet

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import IconBtn from "./components/button/IconBtn";
 import MessageBtn from "./components/button/Message";
 import PrimaryBtn from "./components/button/Primary";
 import SecondaryBtn from "./components/button/Seconday";
+import FormComponentsDemo from "./components/Input/form";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">Mon Application</h1>
+      <FormComponentsDemo />
+      {/* <h1 className="text-3xl font-bold text-gray-800 mb-4">Mon Application</h1>
       <h4 className=" text-black font-bold text-[20px]">
         Primary button type 1
       </h4>
@@ -29,7 +31,7 @@ function App() {
       <MessageBtn />
       <hr />
       <br />
-      <IconBtn />
+      <IconBtn /> */}
     </div>
   );
 }
